4df4a812938e2589a543ffa0857c9322a1a7ae65
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2018-02-08 12:13:45 +0300

    Initial commit

---
+++ start.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+exec docker-entrypoint.sh rabbitmq-server &
+ENTRYPOINT_PID=$!
+
+trap 'kill -TERM $ENTRYPOINT_PID' SIGTERM
+
+RABBIT_INITIALIZED_LOCK=/run/rabbit_initialized.lock
+if [ ! -f $RABBIT_INITIALIZED_LOCK ]; then
+    touch $RABBIT_INITIALIZED_LOCK
+    echo 'Waiting for rabbitmq to start...'
+    sleep $STARTUP_TIMEOUT
+    echo 'Initializing rabbitmq vhost and user...'
+    rabbitmqctl add_user $RABBIT_USER $RABBIT_PASSWORD
+    rabbitmqctl add_vhost $RABBIT_VHOST
+    rabbitmqctl set_permissions -p $RABBIT_VHOST $RABBIT_USER ".*" ".*" ".*"
+fi
+
+wait $ENTRYPOINT_PID
